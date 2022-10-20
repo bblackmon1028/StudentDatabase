0bbbf4fee6dbc783da26e8390e640bf9bd731c9c
Blackmon (WRIGHT), Brittany <BrittanyBlackmon@rocketmortgage.com>
2022-10-20 13:28:12 -0400

    Update with bonus questions

--- StudentDatabase/Program.cs
+++ StudentDatabase/Program.cs
@@ -2,57 +2,63 @@
 string[] homeTown = new string[] { "Detroit", "Romulus", "Grand Rapids", "Livonia", "Garden City", "Westland", "Wayne" };
 string[] favoriteFoods = new string[] { "pizza", "nachos", "chicken tenders", "tacos", "alfredo", "fries", "mac n cheese" };
 
-bool repeat = false;
+bool repeat;
 
 do
 {
-    Console.WriteLine($"Here are our students:");
-    for (int i = 0; i < studentNames.Length; i++)
-    {
-        Console.WriteLine($"{i + 1}. {studentNames[i]}");
-    }
-    Console.WriteLine("Please enter the number of the student you would like information about:");
+    Console.WriteLine("Please enter the number of the student you would like information about. " +
+        "\nIf you would like to see a list of students please press 9.");
     int studentNumber = Convert.ToInt32(Console.ReadLine());
 
     if (studentNumber < 1 || studentNumber > studentNames.Length)
     {
-        do
+        if (studentNumber == 9)
+        {
+            Console.WriteLine($"Here are our students:");
+            for (int i = 0; i < studentNames.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {studentNames[i]}");
+            }
+            Console.WriteLine("Please enter the number of the student you would like information about:");
+            studentNumber = Convert.ToInt32(Console.ReadLine());
+        }
+
+        while (studentNumber < 1 || studentNumber > studentNames.Length)
         {
             Console.WriteLine($"That is not a valid number. Please enter a number between 1 and {studentNames.Length}.");
             studentNumber = Convert.ToInt32(Console.ReadLine());
         }
-        while (studentNumber < 1 || studentNumber > studentNames.Length);
     }
 
     Console.WriteLine($"Students Name: {studentNames[studentNumber - 1]}");
 
-    bool tryAgain = false;
+    bool invalidResponse;
     do
     {
         Console.WriteLine("Would you like to display the students hometown or favorite food?");
         string answer = Console.ReadLine().ToLower();
-        if (answer == "hometown")
+        if (answer == "hometown" || answer.Contains("home") || answer.Contains("town"))
         {
             Console.WriteLine($"{studentNames[studentNumber - 1]}'s hometown is {homeTown[studentNumber - 1]}.");
-            tryAgain = false;
+            invalidResponse = false;
         }
-        else if (answer == "favorite food")
+        else if (answer == "favorite food" || answer.Contains("food") || answer.Contains("favorite"))
         {
             Console.WriteLine($"{studentNames[studentNumber - 1]}'s favorite food is {favoriteFoods[studentNumber - 1]}.");
-            tryAgain = false;
+            invalidResponse = false;
         }
         else
         {
             Console.WriteLine("That is not a valid response. Let's try again.");
-            tryAgain = true;
+            invalidResponse = true;
         }
     }
-    while (tryAgain);
+    while (invalidResponse);
 
     Console.WriteLine("Would you like to learn about another student? (y/n)");
     string userAnswer = Console.ReadLine().ToLower();
 
-    if (userAnswer == "y")
+    if (userAnswer == "y" || userAnswer == "yes")
     {
         repeat = true;
     }
